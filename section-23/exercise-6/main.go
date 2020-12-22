ab5e83f9c34236dfb5079c3cb22bc79bb5d96522
jeremy-spitzig <jpspitzig@gmail.com>
2020-12-22 14:18:12 -0300

    Add exercise 23-6

---
+++ section-23/exercise-6/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+
+	go send(c)
+	receive(c)
+
+	fmt.Println("Exiting")
+}
+
+func send(c chan<- int) {
+	for i := 0; i < 100; i++ {
+		c <- i
+	}
+	close(c)
+}
+
+func receive(c <-chan int) {
+	for v := range c {
+		fmt.Println(v)
+	}
+}
