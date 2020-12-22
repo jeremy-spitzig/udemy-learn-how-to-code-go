7fb1ed3cd7f15d4d26e47d9a532c4913d4d92b13
jeremy-spitzig <jpspitzig@gmail.com>
2020-12-22 14:10:20 -0300

    Add exercise 23-4

---
+++ section-23/exercise-4/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	q := make(chan int)
+	c := gen(q)
+
+	receive(c, q)
+
+	fmt.Println("about to exit")
+}
+
+func receive(c, q <-chan int) {
+	for {
+		select {
+		case <-q:
+			return
+		case v := <-c:
+			fmt.Println(v)
+		}
+	}
+}
+
+func gen(q chan<- int) <-chan int {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		q <- 0
+	}()
+
+	return c
+}
